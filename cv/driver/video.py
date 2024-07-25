466913a4de302c6c27023dd93333d445104c115a
ericbian22 <y24bian@uwaterloo.ca>
2024-07-25 17:17:12 -0400

    finalized Driver side CV

--- cv/driver/video.py
+++ cv/driver/video.py
@@ -2,11 +2,10 @@ import os
 import boto3
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load environment variables
 load_dotenv()
 
 def download_video(bucket_name, file_name, local_directory):
-    # Set up S3 client using environment variables
     s3 = boto3.client(
         's3',
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
@@ -14,15 +13,15 @@ def download_video(bucket_name, file_name, local_directory):
         region_name=os.getenv('AWS_REGION')
     )
 
-    # Ensure the local directory exists
+    
     if not os.path.exists(local_directory):
         os.makedirs(local_directory)
 
-    # Construct the full local path
+
     local_path = os.path.join(local_directory, file_name)
     print(f"Downloading {file_name} to {local_path}...")
 
-    # Download the file
+  
     s3.download_file(bucket_name, file_name, local_path)
     print(f"Downloaded {file_name}")
 
